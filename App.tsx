20b1568d20ed7732fe0ade196ead51aae29aa738
Kostianytsia.Konstantin <kostyantyn.kostyanytsya@hneu.net>
2022-03-31 21:28:20 +0300

    Set up Google Signin

--- App.tsx
+++ App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {RootStack} from './src/navigation/rootStack';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
 const App = () => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      iosClientId:
+        '84423765352-d9laqf200kevh40o1as0ufof4plk7lu2.apps.googleusercontent.com',
+    });
+  }, []);
+
   return (
     <NavigationContainer>
       <RootStack />
--- src/screens/auth/Authorization.tsx
+++ src/screens/auth/Authorization.tsx
@@ -1,12 +1,37 @@
-import translations from 'localization';
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
 
 const AuthorizationScreen = () => {
+  const onPressSignWithGoogle = () => {
+    GoogleSignin.hasPlayServices()
+      .then(hasPlayService => {
+        if (hasPlayService) {
+          GoogleSignin.signIn()
+            .then(userInfo => {
+              console.log(JSON.stringify(userInfo));
+            })
+            .catch(e => {
+              console.log('ERROR IS: ' + JSON.stringify(e));
+            });
+        }
+      })
+      .catch(e => {
+        console.log('ERROR IS: ' + JSON.stringify(e));
+      });
+  };
+
   return (
     <View>
-      <Text>Authorization</Text>
-      <Text>{translations.login}</Text>
+      <GoogleSigninButton
+        style={{width: 192, height: 48}}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={onPressSignWithGoogle}
+      />
     </View>
   );
 };
